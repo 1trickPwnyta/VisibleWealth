82e1f5025fe0e65179b209d8eeaaaf6130077298
1trickPwnyta <sarahpace37@gmail.com>
2025-01-31 19:09:42 -0800

    Make empty window

---
+++ 1.5/Source/Dialog_WealthBreakdown.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VisibleWealth
+{
+    public class Dialog_WealthBreakdown : Window
+    {
+        public override Vector2 InitialSize => new Vector2(600f, 600f);
+
+        public Dialog_WealthBreakdown() : base() 
+        {
+            doCloseButton = true;
+            doCloseX = true;
+            closeOnClickedOutside = true;
+            forcePause = true;
+            optionalTitle = "VisibleWealth_WealthBreakdown".Translate();
+        }
+
+        public override void DoWindowContents(Rect inRect)
+        {
+            
+        }
+    }
+}
